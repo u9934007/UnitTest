9d7f14303fcd8e0b2f34791706f2fa32f1ed25d3
Tsai-Ting Yang <u9934007@gmail.com>
2017-08-07 10:05:10 +0800

    init

---
+++ UnitTest/UnitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UnitTest
+//
+//  Created by 楊采庭 on 2017/8/7.
+//  Copyright © 2017年 楊采庭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
